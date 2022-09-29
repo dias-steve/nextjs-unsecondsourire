781b6f601704d349972fe696ce27a00e769e05e9
Steve Dias <stevediaspro@gmail.com>
2022-09-29 14:26:33 +0200

    [ADD][Container] fonct accessAlowed

--- components/Container/Container.js
+++ components/Container/Container.js
@@ -1,11 +1,33 @@
 import React from "react";
+import EnMaintenancePage from "../EnMaintenance/EnMaintenancePage";
 
 export default function Container(props) {
+  const generalSettings= props.children.props.generalSettings;
+  const maintenanceMode = generalSettings ? generalSettings.maintenance_mode : false;
+  console.log(maintenanceMode);
+
+  const isAccessAllowed = () => {
+    if (maintenanceMode?.is_activated){
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+
   return (
+   <>
+    { !isAccessAllowed() ?  
+    
+    <EnMaintenancePage />:     
+    
     <>
       <p>Header</p>
+   
       {props.children}
       <p>footer</p>
+    </> }
+
     </>
   );
 }
