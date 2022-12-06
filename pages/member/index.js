ab27f0f9cd17db5f392103346bdd4ff961ea8eb6
Steve Dias <stevediaspro@gmail.com>
2022-12-06 15:31:05 +0100

    [ADD][MEMBERPAGE] implementing of the memeber page

---
+++ pages/member/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './MemberPage.module.scss'
+export default function MemberPage() {
+  return (
+    <div className={styles.global_container}>
+      
+    </div>
+  )
+}
